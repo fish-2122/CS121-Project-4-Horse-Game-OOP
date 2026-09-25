8dac0199e574aa96b3a6b748e549e9ed06f73539
fish-2122 <keiner.rinehart@bsu.edu>
2026-09-25 17:48:21 +0000

    continued Race

--- Race.cpp
+++ Race.cpp
@@ -7,5 +7,20 @@ Race::Race() {
 }
 
 Race::start() {
-	
+	for (int i = 0; i < NUM_HORSES; i++) {
+		Race::horses[i].printLane();
+	}
+	std::cout << std::endl;
+	bool keepGoing = true;
+	while (keepGoing) {
+		bool isThereAWinner = false;
+		
+		for (int horse = 0; horse < NUM_HORSES; horse++) {
+			
+		}
+
+		if (isThereAWinner) {
+			keepGoing = false;
+		}
+	}
 }
